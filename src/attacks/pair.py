a55465b2656accadadf085f2c1cb0d57c95984f2
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2025-03-02 23:12:20 -0500

    reorder classes

--- src/attacks/pair.py
+++ src/attacks/pair.py
@@ -70,127 +70,6 @@ class _AttackSinglePromptResult:
     input_toks: list = field(default_factory=list)
     completions_toks: list = field(default_factory=list)
 
-class PAIRAttack(Attack):
-    def __init__(self, config):
-        super().__init__(config)
-
-    def run(
-        self,
-        model: AutoModelForCausalLM,
-        tokenizer: AutoTokenizer,
-        dataset: torch.utils.data.Dataset,
-        log_full_results: bool = False,
-    ) -> AttackResult:
-
-        # Initialize models 
-        target_model = HuggingFace(model, tokenizer)
-
-        # Can share underlying model and save VRAM if attack & target model are the same
-        if self.config.attack_model.id == model.model.name_or_path:
-            attack_model, attack_tokenizer = target_model, tokenizer
-        else:
-            attack_model, attack_tokenizer = load_model_and_tokenizer(self.config.attack_model)
-            attack_model = HuggingFace(attack_model, attack_tokenizer)
-
-        target_model = TargetLM(target_model, tokenizer, self.config.target_model)
-        attack_model = AttackLM(attack_model, attack_tokenizer, self.config.attack_model)
-
-        results = AttackResult([], [], [], [], [], [], [])
-
-        for msg, target in dataset:
-            # prepare tokens
-            output = self.attack_single_prompt(attack_model, target_model, msg["content"], target)
-            attacks, completions, times = output.attacks, output.completions, output.times
-            results.prompts.append(msg)
-            results.attacks.append(attacks)
-            results.completions.append(completions)
-            results.times.append(times)
-            results.losses.append([None] * len(attacks))
-
-            if log_full_results:
-                results.completions_toks.append(output.completions_toks)
-                results.input_toks.append(output.input_toks)
-            
-        return results
-
-    def attack_single_prompt(
-        self, 
-        attack_model: AttackLM,
-        target_model: TargetLM,
-        prompt, 
-        target
-    ) -> _AttackSinglePromptResult:
-        attacks = []
-        completions = []
-        times = []
-
-        # tokens to save
-        input_toks = []
-        completions_toks = []
-
-        t0 = time.time()
-        # TODO: early stopping with judge model
-        # judgeLM = load_judge(args)
-
-        # Initialize conversations
-        init_msg = INIT_MSG.format(prompt=prompt, target=target)
-        processed_response_list = [init_msg for _ in range(self.config.num_streams)]
-
-        system_prompt = ATTACKER_SYSTEM_PROMPT.format(prompt=prompt, target=target)
-        convs_list = [
-            [{"role": "system", "content": system_prompt}]
-            for _ in range(self.config.num_streams)
-        ]
-
-        # Begin PAIR
-        for _ in trange(self.config.num_steps):
-            # Get adversarial prompts and improvement
-            extracted_attack_list = attack_model.get_attack(
-                convs_list, processed_response_list
-            )
-            if any([attack is None for attack in extracted_attack_list]):
-                logging.info("Failed to generate output. Terminating.")
-                break
-            logging.info("Finished getting adversarial prompts.")
-
-            # Extract prompts and improvements
-            adv_prompt_list = [attack["prompt"] for attack in extracted_attack_list]
-            attacks.extend(adv_prompt_list)
-            improv_list = [attack["improvement"] for attack in extracted_attack_list]
-
-            # Get target responses 
-            # TODO: return all the data we want HERE
-            times.append(time.time() - t0)
-            target_output = target_model.get_response(adv_prompt_list)
-            target_response_list = target_output.outputs_list
-
-            completions.extend(target_response_list)
-            input_toks.extend(target_output.input_ids_list)
-            completions_toks.extend(target_output.outputs_ids_list)
-            logging.info("Finished getting target responses.")
-
-            # Get judge scores
-            judge_scores = [1] * len(target_response_list)
-            # TODO: early stopping with judge model
-            # judgeLM.score(adv_prompt_list, target_response_list)
-            processed_response_list = [
-                process_target_response(target_response, score, prompt)
-                for target_response, score in zip(target_response_list, judge_scores)
-            ]
-            # TODO: early stopping with judge model
-            # Early stopping criterion
-            # if any([score == 10 for score in judge_scores]):
-            #     logging.info("Found a jailbreak. Exiting.")
-            #     break
-
-        return _AttackSinglePromptResult(
-            attacks=attacks, 
-            completions=completions, 
-            times=times, 
-            input_toks=input_toks, 
-            completions_toks=completions_toks
-        )
-
 
 def fix_llama2_tokens(inputs):
     target_sequence = torch.tensor([518, 29914, 25580, 29962])
@@ -486,6 +365,128 @@ class TargetLM:
         # outputs_list = outputs.outputs_list
 
         return outputs
+
+
+class PAIRAttack(Attack):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def run(
+        self,
+        model: AutoModelForCausalLM,
+        tokenizer: AutoTokenizer,
+        dataset: torch.utils.data.Dataset,
+        log_full_results: bool = False,
+    ) -> AttackResult:
+
+        # Initialize models 
+        target_model = HuggingFace(model, tokenizer)
+
+        # Can share underlying model and save VRAM if attack & target model are the same
+        if self.config.attack_model.id == model.model.name_or_path:
+            attack_model, attack_tokenizer = target_model, tokenizer
+        else:
+            attack_model, attack_tokenizer = load_model_and_tokenizer(self.config.attack_model)
+            attack_model = HuggingFace(attack_model, attack_tokenizer)
+
+        target_model = TargetLM(target_model, tokenizer, self.config.target_model)
+        attack_model = AttackLM(attack_model, attack_tokenizer, self.config.attack_model)
+
+        results = AttackResult([], [], [], [], [], [], [])
+
+        for msg, target in dataset:
+            # prepare tokens
+            output = self.attack_single_prompt(attack_model, target_model, msg["content"], target)
+            attacks, completions, times = output.attacks, output.completions, output.times
+            results.prompts.append(msg)
+            results.attacks.append(attacks)
+            results.completions.append(completions)
+            results.times.append(times)
+            results.losses.append([None] * len(attacks))
+
+            if log_full_results:
+                results.completions_toks.append(output.completions_toks)
+                results.input_toks.append(output.input_toks)
+            
+        return results
+
+    def attack_single_prompt(
+        self, 
+        attack_model: AttackLM,
+        target_model: TargetLM,
+        prompt, 
+        target
+    ) -> _AttackSinglePromptResult:
+        attacks = []
+        completions = []
+        times = []
+
+        # tokens to save
+        input_toks = []
+        completions_toks = []
+
+        t0 = time.time()
+        # TODO: early stopping with judge model
+        # judgeLM = load_judge(args)
+
+        # Initialize conversations
+        init_msg = INIT_MSG.format(prompt=prompt, target=target)
+        processed_response_list = [init_msg for _ in range(self.config.num_streams)]
+
+        system_prompt = ATTACKER_SYSTEM_PROMPT.format(prompt=prompt, target=target)
+        convs_list = [
+            [{"role": "system", "content": system_prompt}]
+            for _ in range(self.config.num_streams)
+        ]
+
+        # Begin PAIR
+        for _ in trange(self.config.num_steps):
+            # Get adversarial prompts and improvement
+            extracted_attack_list = attack_model.get_attack(
+                convs_list, processed_response_list
+            )
+            if any([attack is None for attack in extracted_attack_list]):
+                logging.info("Failed to generate output. Terminating.")
+                break
+            logging.info("Finished getting adversarial prompts.")
+
+            # Extract prompts and improvements
+            adv_prompt_list = [attack["prompt"] for attack in extracted_attack_list]
+            attacks.extend(adv_prompt_list)
+            improv_list = [attack["improvement"] for attack in extracted_attack_list]
+
+            # Get target responses 
+            # TODO: return all the data we want HERE
+            times.append(time.time() - t0)
+            target_output = target_model.get_response(adv_prompt_list)
+            target_response_list = target_output.outputs_list
+
+            completions.extend(target_response_list)
+            input_toks.extend(target_output.input_ids_list)
+            completions_toks.extend(target_output.outputs_ids_list)
+            logging.info("Finished getting target responses.")
+
+            # Get judge scores
+            judge_scores = [1] * len(target_response_list)
+            # TODO: early stopping with judge model
+            # judgeLM.score(adv_prompt_list, target_response_list)
+            processed_response_list = [
+                process_target_response(target_response, score, prompt)
+                for target_response, score in zip(target_response_list, judge_scores)
+            ]
+            # TODO: early stopping with judge model
+            # Early stopping criterion
+            # if any([score == 10 for score in judge_scores]):
+            #     logging.info("Found a jailbreak. Exiting.")
+            #     break
+
+        return _AttackSinglePromptResult(
+            attacks=attacks, 
+            completions=completions, 
+            times=times, 
+            input_toks=input_toks, 
+            completions_toks=completions_toks
+        )
 
 
 def process_target_response(target_response, score, goal):
